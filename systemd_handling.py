04bec8b05b88455d6cae390ab6533a5ae5e7719d
troelf112 <troelf.jf.klingenthal@gmail.com>
2026-06-04 01:00:48 +0200

    added setup of systemd device for selected config for keyboard rgb

---
+++ systemd_handling.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import subprocess
+
+service_file = "/etc/systemd/system/rgb_controller.service"
+
+if Path(service_file).is_file():
+    with open(service_file, "r") as f:
+        file_content = f.read()
+else:
+    file_content = '''[Unit]
+Description=keyboard rgb controller service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /home/jakob/Dokumente/Programmieren/Python/keyboard_light/main.py
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target'''
+
+def update_system() -> None:
+    subprocess.run(["systemctl", "daemon-reload"])
+    subprocess.run(["systemctl", "enable", "rgb_controller.service"])
+    subprocess.run(["systemctl", "restart", "rgb_controller.service"])
+
+def update_service(command:str) -> None:
+    global file_content
+    file_content = file_content.split("\n")
+    for i,line in enumerate(file_content):
+        if line.startswith("ExecStart"):
+            file_content[i] = line[:line.index('=') + 1] + command
+            break
+
+    with open(service_file, "w") as f:
+        f.write('\n'.join(file_content))
+
+    update_system()
